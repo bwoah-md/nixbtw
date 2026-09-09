8cd9f8c478886e3c7aa0df01d520a2b8a756ccd6
bwoah-md <143240188+bwoah-md@users.noreply.github.com>
2026-09-09 23:56:50 +0530

    ran nixfrost at $(date '+%-d %b, %Y at %H:%M')

--- users/icy/default.nix
+++ users/icy/default.nix
@@ -8,7 +8,14 @@
     isNormalUser = true;
     description = "icy";
     shell = pkgs.zsh;
-    extraGroups = [ "networkmanager" "wheel" "video" "audio" "docker" "kvm" ];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "video"
+      "audio"
+      "docker"
+      "kvm"
+    ];
   };
 
   programs.fzf = {
@@ -22,6 +29,7 @@
     enable = true;
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
+
     histSize = 10000;
     histFile = "$HOME/.zsh_history";
 
@@ -39,24 +47,33 @@
       # v = "nvim";
       ff = "fastfetch";
 
+      # NixOS
       nixadd     = "git -C ~/.config/nixos add -A";
       nixcommit  = "git -C ~/.config/nixos commit -m";
       nixpush    = "git -C ~/.config/nixos push origin main";
       nixpull    = "git -C ~/.config/nixos pull origin main";
       nixstatus  = "git -C ~/.config/nixos status";
       nixrebuild = "sudo nixos-rebuild switch --flake ~/.config/nixos#nix";
+
       nixupdate  = "cd ~/.config/nixos && nix-update swash --flake --build && nix-update superseedr --flake --build && nix-update ghosttime --flake --build";
+
       nixclean   = "sudo nix-collect-garbage -d && nix-collect-garbage -d";
       nixflake   = "nix flake update --flake ~/.config/nixos";
 
+      # Full NixOS update workflow
+      nixfrost   = "nixupdate && rm -f result && nixflake && nixadd && nixrebuild && nixcommit \"ran nixfrost at \\$(date '+%-d %b, %Y at %H:%M')\" && nixpush && nixclean";
+
+      # Docker
       docker-start = "sudo systemctl start docker";
       docker-stop  = "sudo systemctl stop docker";
 
+      # Windows VM
       win = "sdl-freerdp /u:\"icy\" /p:\"1771\" /v:127.0.0.1:3389 /cert:ignore /dynamic-resolution +clipboard /sound /microphone +home-drive";
 
       win-start = "sudo systemctl start docker && docker start windows";
       win-stop  = "docker stop windows && sudo systemctl stop docker";
 
+      # Phone
       mount-phone  = "mkdir -p ~/LineageOS && sshfs LineageOS:/storage/emulated/0 ~/LineageOS";
       umount-phone = "fusermount -u ~/LineageOS";
     };
@@ -64,28 +81,23 @@
     shellInit = ''
       export PATH="$HOME/.local/bin:$PATH"
       export FZF_BASE="${pkgs.fzf}/share/fzf"
-      [[ -f ~/.config/fzf/themes/noctalia.sh ]] && source ~/.config/fzf/themes/noctalia.sh
+
+      [[ -f ~/.config/fzf/themes/noctalia.sh ]] && \
+        source ~/.config/fzf/themes/noctalia.sh
 
       # Launch Zed completely detached, always opening a new window
       zed() {
         ${pkgs.zed-editor}/libexec/zed-editor -n "$@" >/dev/null 2>&1 &!
       }
-
-      nixfrost() {
-        nixupdate && \
-        rm result && \
-        nixflake && \
-        nixadd && \
-        nixrebuild && \
-        nixcommit "ran nixfrost at $(date '+%-d %b, %Y at %H:%M')" && \
-        nixpush && \
-        nixclean
-      }
     '';
 
     ohMyZsh = {
       enable = true;
-      plugins = [ "git" "sudo" "copypath" ];
+      plugins = [
+        "git"
+        "sudo"
+        "copypath"
+      ];
     };
 
     promptInit = ''
