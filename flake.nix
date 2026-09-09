bd011c442285b8e310790535180845f1567a5c74
bwoah-md <143240188+bwoah-md@users.noreply.github.com>
2026-09-09 16:28:11 +0530

    compositor: mangowm added

--- flake.nix
+++ flake.nix
@@ -1,54 +1,69 @@
 {
   description = "NixOS Flake Configuration for icy@nix";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
+
     noctalia.url = "github:noctalia-dev/noctalia/cachix";
+
     noctalia-greeter = {
       url = "github:noctalia-dev/noctalia-greeter";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     helium-flake = {
       url = "github:oxcl/nix-flake-helium-browser";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     surge.url = "github:SurgeDM/Surge/08d09d11199acf6082a89c4da0d19a04749de997";
+
     winapps = {
       url = "github:winapps-org/winapps";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     umbriel = {
       url = "git+https://github.com/noctalia-dev/umbriel.git?submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    mango = {
+      url = "github:mangowm/mango";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     xdg-desktop-portal-umbriel = {
       url = "git+https://github.com/noctalia-dev/xdg-desktop-portal-umbriel.git?submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, ... }@inputs:
-  let
-    system = "x86_64-linux";
 
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
 
-    pkgsStable = import inputs.nixpkgs-stable {
-      inherit system;
-      config.allowUnfree = true;
-    };
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
 
-    customPackages = import ./modules/packages/custom.nix {
-      inherit pkgs;
-      lib = pkgs.lib;
-    };
-  in
+      customPackages = import ./modules/packages/custom.nix {
+        inherit pkgs;
+        lib = pkgs.lib;
+      };
+    in
     {
       nixosConfigurations.nix = nixpkgs.lib.nixosSystem {
         inherit system;
-        specialArgs = { inherit inputs; pkgsStable = pkgsStable; };
+
+        specialArgs = {
+          inherit inputs;
+        };
+
         modules = [
           ./hosts/nix
           inputs.noctalia.nixosModules.default
--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     inputs.umbriel.nixosModules.default
+    inputs.mango.nixosModules.mango
     ./greeter.nix
     ./theming.nix
   ];
@@ -9,6 +10,9 @@
     enable = true;
     # package = pkgs.umbriel;
     # portalPackage = pkgs.xdg-desktop-portal-umbriel;
+  };
+  programs.mango = {
+    enable = true;
   };
 
   # Noctalia shell and desktop environment integration
