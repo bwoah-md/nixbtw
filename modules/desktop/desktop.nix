fd06c86a705045a04651695ac950f2cc7e5f697b
bwoah-md <143240188+bwoah-md@users.noreply.github.com>
2026-09-18 02:18:45 +0530

    portal: desktop-portal-wlr for labwc

--- modules/desktop/desktop.nix
+++ modules/desktop/desktop.nix
@@ -29,6 +29,14 @@
       xdg-desktop-portal-wlr
     ];
 
+    wlr = {
+      enable = true;
+      settings.screencast = {
+        chooser_type = "simple";
+        chooser_cmd = "${pkgs.slurp}/bin/slurp -f 'Monitor: %o' -or";
+      };
+    };
+
     config = {
       umbriel = {
         default = [ "gtk" ];
